9313595ffae40fb986cd8e22358205e3bd944579
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-06-27 17:03:04 +0530

    add alt text

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <main>
       <section className="p-4">
         <div className="max-w-2xs space-y-4 rounded-xl bg-white p-4 text-center">
-          <img className="rounded-xl" src="./assets/image-qr-code.png" />
+          <img className="rounded-xl" src="./assets/image-qr-code.png" alt="QR Code Component" />
           <h1 className="font-bold text-slate-900">
             Improve your front-end skills by building projects
           </h1>
